b140c84c0499bd084d3bfb47202563750ed9da62
rousong <rousong@7lk.com>
2018-04-13 13:45:47 +0800

    413

--- hansong/4.23mysql/mysql.js
+++ hansong/4.23mysql/mysql.js
@@ -466,5 +466,446 @@ Max_data_length: 25769803775
 修改表名
 如果需要修改数据表的名称，可以在 ALTER TABLE 语句中使用 RENAME 子句来实现。
 尝试以下实例将数据表 testalter_tbl 重命名为 alter_tbl：
-ALTER TABLE test_tbl RENAME TO alert_tbl;
+// ALTER TABLE test_tbl RENAME TO alert_tbl;
 
+索引分单列索引和组合索引。单列索引，即一个索引只包含单个列，一个表可以有多个单列索引，但这不是组合索引。组合索引，即一个索引包含多个列。
+
+创建索引时，你需要确保该索引是应用在	SQL 查询语句的条件(一般作为 WHERE 子句的条件)。
+实际上，索引也是一张表，该表保存了主键与索引字段，并指向实体表的记录。
+上面都在说使用索引的好处，但过多的使用索引将会造成滥用。因此索引也会有它的缺点：虽然索引大大提高了查询速度，同时却会降低更新表的速度，如对表进行INSERT、UPDATE和DELETE。因为更新表时，MySQL不仅要保存数据，还要保存一下索引文件。
+建立索引会占用磁盘空间的索引文件。
+
+
+普通索引
+创建索引
+// CREATE INDEX indexName ON mytable(username(length));
+如果是CHAR，VARCHAR类型，length可以小于字段实际长度；如果是BLOB和TEXT类型，必须指定 length。
+
+修改表结构(添加索引)
+// ALTER table tablename ADD INDEX indexname(cloumnName);
+
+创建表的时候直接指定
+// CREATE TABLE mytable(
+//     ID INT NOT NULL,
+//     usernaem VARCHAR(16) NOT NULL,
+//     INDEX [indexName] (username(length))
+// )
+
+删除索引的语法
+DROP INDEX [indexName] ON mytable;
+
+唯一索引
+它与前面的普通索引类似，不同的就是：索引列的值必须唯一，但允许有空值。如果是组合索引，则列值的组合必须唯一。它有以下几种创建方式：
+
+// 创建索引
+CREATE UNIQUE INDEX indexName ON mytable(username(length));
+
+// 修改表结构
+ALTER table mytable ADD UNIQUE [indexName] (username(length));
+
+创建表的时候直接指定
+CREATE TABLE mytable(
+    ID INT NOT NULL,
+    username VARCHAR(16) NOT NULL,
+    UNIQUE [indexName] (username(length)) 
+);
+
+// 使用ALTER 命令添加和删除索引
+
+有四种方式来添加数据表的索引：
+ALTER TABLE tbl_name ADD PRIMARY KEY (column_list): 该语句添加一个主键，这意味着索引值必须是唯一的，且不能为NULL。
+ALTER TABLE tbl_name ADD UNIQUE index_name (column_list): 这条语句创建索引的值必须是唯一的（除了NULL外，NULL可能会出现多次）。
+ALTER TABLE tbl_name ADD INDEX index_name (column_list): 添加普通索引，索引值可出现多次。
+ALTER TABLE tbl_name ADD FULLTEXT index_name (column_list):该语句指定了索引为 FULLTEXT ，用于全文索引。
+以下实例为在表中添加索引。
+ALTER TABLE test_tbl ADD INDEX (c);
+你还可以在 ALTER 命令中使用 DROP 子句来删除索引。尝试以下实例删除索引:
+ALTER TABLE test_tbl DROP INDEX c;
+
+使用 ALTER 命令添加和删除主键
+ALTER TABLE test_tbl MODIFY i INT NOT NUll;
+ALTER TABLE test_tbl ADD PRIMARY KEY(i);
+
+
+你也可以使用 ALTER 命令删除主键：
+
+// ALTER TABLE test_tbl DROP PRIMARY KEY;
+
+删除主键时只需指定PRIMARYaidu KEY，但在删除索引时，你必须知道索引名。
+
+显示索引信息
+你可以使用 SHOW INDEX 命令来列出表中的相关的索引信息。可以通过添加 \G 来格式化输出信息。
+SHOW INDEX FROM table_name; \G
+
+
+MySQL 临时表
+MySQL 临时表在我们需要保存一些临时数据时是非常有用的。临时表只在当前连接可见，当关闭连接时，Mysql会自动删除表并释放所有空间。
+CREATE TEMPORARY TABLE SalesSummary (
+    product_name VARCHAR(50) NOT NULL,
+    total_sales DECIMAL(12,2) NOT NULL DEFAULT 0.00,
+    avg_unit_price DECIMAL(7,2) NOT NULL DEFAULT 0.00,
+    total_units INT UNSIGNED NOT NULL DEFAULT 0
+)
+当你使用 SHOW TABLES命令显示数据表列表时，你将无法看到 SalesSummary表。
+如果你退出当前MySQL会话，再使用 SELECT命令来读取原先创建的临时表数据，那你会发现数据库中没有该表的存在，因为在你退出时该临时表已经被销毁了。
+
+删除MySQL 临时表
+默认情况下，当你断开与数据库的连接后，临时表就会自动被销毁。当然你也可以在当前MySQL会话使用 DROP TABLE 命令来手动删除临时表。
+以下是手动删除临时表的实例：
+
+DROP TABLE SalesSummary;
+
+MySQL 复制表
+步骤一：
+获取数据表的完整结构。
+mysql> SHOW CREATE TABLE runoob_tbl \G;
+*************************** 1. row ***************************
+       Table: runoob_tbl
+Create Table: CREATE TABLE `runoob_tbl` (
+  `runoob_id` int(11) NOT NULL auto_increment,
+  `runoob_title` varchar(100) NOT NULL default '',
+  `runoob_author` varchar(40) NOT NULL default '',
+  `submission_date` date default NULL,
+  PRIMARY KEY  (`runoob_id`),
+  UNIQUE KEY `AUTHOR_INDEX` (`runoob_author`)
+) ENGINE=InnoDB 
+1 row in set (0.00 sec)
+
+ERROR:
+No query specified
+步骤二：
+修改SQL语句的数据表名，并执行SQL语句。
+mysql> CREATE TABLE `clone_tbl` (
+  -> `runoob_id` int(11) NOT NULL auto_increment,
+  -> `runoob_title` varchar(100) NOT NULL default '',
+  -> `runoob_author` varchar(40) NOT NULL default '',
+  -> `submission_date` date default NULL,
+  -> PRIMARY KEY  (`runoob_id`),
+  -> UNIQUE KEY `AUTHOR_INDEX` (`runoob_author`)
+-> ) ENGINE=InnoDB;
+Query OK, 0 rows affected (1.80 sec)
+步骤三：
+执行完第二步骤后，你将在数据库中创建新的克隆表 clone_tbl。 如果你想拷贝数据表的数据你可以使用 INSERT INTO... SELECT 语句来实现。
+INSERT INTO clone_tbl(runoob_id,runoob_title,runoob_author,submission_date) SELECT runoob_id,runoob_title, runoob_author,submission_date FROM runoob_tbl;
+
+
+MySQL 元数据
+你可能想知道MySQL以下三种信息：
+查询结果信息： SELECT, UPDATE 或 DELETE语句影响的记录数。
+数据库和数据表的信息： 包含了数据库及数据表的结构信息。
+MySQL服务器信息： 包含了数据库服务器的当前状态，版本号等。
+在MySQL的命令提示符中，我们可以很容易的获取以上服务器信息。 但如果使用Perl或PHP等脚本语言，你就需要调用特定的接口函数来获取。 接下来我们会详细介绍。
+获取查询语句影响的记录数
+PERL 实例
+在 DBI 脚本中， 语句影响的记录数通过函数 do( ) 或 execute( )返回：
+# 方法 1
+# 使用do( ) 执行  $query 
+my $count = $dbh->do ($query);
+# 如果发生错误会输出 0
+printf "%d 条数据被影响\n", (defined ($count) ? $count : 0);
+
+# 方法 2
+# 使用prepare( ) 及 execute( ) 执行  $query 
+my $sth = $dbh->prepare ($query);
+my $count = $sth->execute ( );
+printf "%d 条数据被影响\n", (defined ($count) ? $count : 0);
+
+
+MySQL 序列使用
+MySQL序列是一组整数：1, 2, 3, ...，由于一张数据表只能有一个字段自增主键， 如果你想实现其他字段也实现自动增加，就可以使用MySQL序列来实现。
+mysql> CREATE TABLE insect
+    -> (
+    -> id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    -> PRIMARY KEY (id),
+    -> name VARCHAR(30) NOT NULL, # type of insect
+    -> date DATE NOT NULL, # date collected
+    -> origin VARCHAR(30) NOT NULL # where collected
+);
+Query OK, 0 rows affected (0.02 sec)
+mysql> INSERT INTO insect (id,name,date,origin) VALUES
+    -> (NULL,'housefly','2001-09-10','kitchen'),
+    -> (NULL,'millipede','2001-09-10','driveway'),
+    -> (NULL,'grasshopper','2001-09-10','front yard');
+Query OK, 3 rows affected (0.02 sec)
+Records: 3  Duplicates: 0  Warnings: 0
+mysql> SELECT * FROM insect ORDER BY id;
++----+-------------+------------+------------+
+| id | name        | date       | origin     |
++----+-------------+------------+------------+
+|  1 | housefly    | 2001-09-10 | kitchen    |
+|  2 | millipede   | 2001-09-10 | driveway   |
+|  3 | grasshopper | 2001-09-10 | front yard |
++----+-------------+------------+------------+
+3 rows in set (0.00 sec)
+
+重置序列
+mysql> ALTER TABLE insect DROP id;
+mysql> ALTER TABLE insect
+    -> ADD id INT UNSIGNED NOT NULL AUTO_INCREMENT FIRST,
+    -> ADD PRIMARY KEY (id);
+
+
+    设置序列的开始值
+一般情况下序列的开始值为1，但如果你需要指定一个开始值100，那我们可以通过以下语句来实现：
+mysql> CREATE TABLE insect
+    -> (
+    -> id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    -> PRIMARY KEY (id),
+    -> name VARCHAR(30) NOT NULL, 
+    -> date DATE NOT NULL,
+    -> origin VARCHAR(30) NOT NULL
+)engine=innodb auto_increment=100 charset=utf8;
+或者你也可以在表创建成功后，通过以下语句来实现：
+mysql> ALTER TABLE t AUTO_INCREMENT = 100;
+
+防止表中出现重复数据
+你可以在MySQL数据表中设置指定的字段为 PRIMARY KEY（主键） 或者 UNIQUE（唯一） 索引来保证数据的唯一性。
+
+
+MySQL 处理重复数据
+有些 MySQL 数据表中可能存在重复的记录，有些情况我们允许重复数据的存在，但有时候我们也需要删除这些重复的数据。
+本章节我们将为大家介绍如何防止数据表出现重复数据及如何删除数据表中的重复数据。
+防止表中出现重复数据
+你可以在MySQL数据表中设置指定的字段为 PRIMARY KEY（主键） 或者 UNIQUE（唯一） 索引来保证数据的唯一性。
+让我们尝试一个实例：下表中无索引及主键，所以该表允许出现多条重复记录。
+CREATE TABLE person_tbl
+(
+    first_name CHAR(20),
+    last_name CHAR(20),
+    sex CHAR(10)
+);
+如果你想设置表中字段first_name，last_name数据不能重复，你可以设置双主键模式来设置数据的唯一性， 如果你设置了双主键，那么那个键的默认值不能为NULL，可设置为NOT NULL。如下所示：
+CREATE TABLE person_tbl
+(
+   first_name CHAR(20) NOT NULL,
+   last_name CHAR(20) NOT NULL,
+   sex CHAR(10),
+   PRIMARY KEY (last_name, first_name)
+);
+如果我们设置了唯一索引，那么在插入重复数据时，SQL语句将无法执行成功,并抛出错。
+INSERT IGNORE INTO与INSERT INTO的区别就是INSERT IGNORE会忽略数据库中已经存在的数据，如果数据库没有数据，就插入新的数据，如果有数据的话就跳过这条数据。这样就可以保留数据库中已经存在数据，达到在间隙中插入数据的目的。
+以下实例使用了INSERT IGNORE INTO，执行后不会出错，也不会向数据表中插入重复数据：
+mysql> INSERT IGNORE INTO person_tbl (last_name, first_name)
+    -> VALUES( 'Jay', 'Thomas');
+Query OK, 1 row affected (0.00 sec)
+mysql> INSERT IGNORE INTO person_tbl (last_name, first_name)
+    -> VALUES( 'Jay', 'Thomas');
+Query OK, 0 rows affected (0.00 sec)
+INSERT IGNORE INTO当插入数据时，在设置了记录的唯一性后，如果插入重复数据，将不返回错误，只以警告形式返回。 而REPLACE INTO into如果存在primary 或 unique相同的记录，则先删除掉。再插入新记录。
+另一种设置数据的唯一性方法是添加一个UNIQUE索引，如下所示：
+CREATE TABLE person_tbl
+(
+   first_name CHAR(20) NOT NULL,
+   last_name CHAR(20) NOT NULL,
+   sex CHAR(10)
+   UNIQUE (last_name, first_name)
+);
+统计重复数据
+以下我们将统计表中 first_name 和 last_name的重复记录数：
+mysql> SELECT COUNT(*) as repetitions, last_name, first_name
+    -> FROM person_tbl
+    -> GROUP BY last_name, first_name
+    -> HAVING repetitions > 1;
+以上查询语句将返回 person_tbl 表中重复的记录数。 一般情况下，查询重复的值，请执行以下操作：
+确定哪一列包含的值可能会重复。
+在列选择列表使用COUNT(*)列出的那些列。
+在GROUP BY子句中列出的列。
+HAVING子句设置重复数大于1。
+过滤重复数据
+如果你需要读取不重复的数据可以在 SELECT 语句中使用 DISTINCT 关键字来过滤重复数据。
+mysql> SELECT DISTINCT last_name, first_name
+    -> FROM person_tbl;
+你也可以使用 GROUP BY 来读取数据表中不重复的数据：
+mysql> SELECT last_name, first_name
+    -> FROM person_tbl
+    -> GROUP BY (last_name, first_name);
+删除重复数据
+如果你想删除数据表中的重复数据，你可以使用以下的SQL语句：
+mysql> CREATE TABLE tmp SELECT last_name, first_name, sex FROM person_tbl  GROUP BY (last_name, first_name, sex);
+mysql> DROP TABLE person_tbl;
+mysql> ALTER TABLE tmp RENAME TO person_tbl;
+当然你也可以在数据表中添加 INDEX（索引） 和 PRIMAY KEY（主键）这种简单的方法来删除表中的重复记录。方法如下：
+mysql> ALTER IGNORE TABLE person_tbl
+    -> ADD PRIMARY KEY (last_name, first_name);
+
+
+
+    MySQL 及 SQL 注入
+
+    MySQL 及 SQL 注入
+如果您通过网页获取用户输入的数据并将其插入一个MySQL数据库，那么就有可能发生SQL注入安全的问题。
+本章节将为大家介绍如何防止SQL注入，并通过脚本来过滤SQL中注入的字符。
+所谓SQL注入，就是通过把SQL命令插入到Web表单递交或输入域名或页面请求的查询字符串，最终达到欺骗服务器执行恶意的SQL命令。
+我们永远不要信任用户的输入，我们必须认定用户输入的数据都是不安全的，我们都需要对用户输入的数据进行过滤处理。
+以下实例中，输入的用户名必须为字母、数字及下划线的组合，且用户名长度为 8 到 20 个字符之间：
+if (preg_match("/^\w{8,20}$/", $_GET['username'], $matches))
+{
+   $result = mysqli_query($conn, "SELECT * FROM users 
+                          WHERE username=$matches[0]");
+}
+ else 
+{
+   echo "username 输入异常";
+}
+让我们看下在没有过滤特殊字符时，出现的SQL情况：
+// 设定$name 中插入了我们不需要的SQL语句
+$name = "Qadir'; DELETE FROM users;";
+ mysqli_query($conn, "SELECT * FROM users WHERE name='{$name}'");
+以上的注入语句中，我们没有对 $name 的变量进行过滤，$name 中插入了我们不需要的SQL语句，将删除 users 表中的所有数据。
+在PHP中的 mysqli_query() 是不允许执行多个 SQL 语句的，但是在 SQLite 和 PostgreSQL 是可以同时执行多条SQL语句的，所以我们对这些用户的数据需要进行严格的验证。
+防止SQL注入，我们需要注意以下几个要点：
+1.永远不要信任用户的输入。对用户的输入进行校验，可以通过正则表达式，或限制长度；对单引号和 双"-"进行转换等。
+2.永远不要使用动态拼装sql，可以使用参数化的sql或者直接使用存储过程进行数据查询存取。
+3.永远不要使用管理员权限的数据库连接，为每个应用使用单独的权限有限的数据库连接。
+4.不要把机密信息直接存放，加密或者hash掉密码和敏感的信息。
+5.应用的异常信息应该给出尽可能少的提示，最好使用自定义的错误信息对原始错误信息进行包装
+6.sql注入的检测方法一般采取辅助软件或网站平台来检测，软件一般采用sql注入检测工具jsky，网站平台就有亿思网站安全平台检测工具。MDCSOFT SCAN等。采用MDCSOFT-IPS可以有效的防御SQL注入，XSS攻击等。
+防止SQL注入
+在脚本语言，如Perl和PHP你可以对用户输入的数据进行转义从而来防止SQL注入。
+PHP的MySQL扩展提供了mysqli_real_escape_string()函数来转义特殊的输入字符。
+if (get_magic_quotes_gpc()) 
+{
+  $name = stripslashes($name);
+}
+$name = mysqli_real_escape_string($conn, $name);
+ mysqli_query($conn, "SELECT * FROM users WHERE name='{$name}'");
+Like语句中的注入
+like查询时，如果用户输入的值有"_"和"%"，则会出现这种情况：用户本来只是想查询"abcd_"，查询结果中却有"abcd_"、"abcde"、"abcdf"等等；用户要查询"30%"（注：百分之三十）时也会出现问题。
+在PHP脚本中我们可以使用addcslashes()函数来处理以上情况，如下实例：
+$sub = addcslashes(mysqli_real_escape_string($conn, "%something_"), "%_");
+// $sub == \%something\_
+ mysqli_query($conn, "SELECT * FROM messages WHERE subject LIKE '{$sub}%'");
+addcslashes() 函数在指定的字符前添加反斜杠。
+语法格式:
+addcslashes(string,characters)
+参数	描述
+string	必需。规定要检查的字符串。
+characters	可选。规定受 addcslashes() 影响的字符或字符范围。
+
+
+
+MySQL 导出数据
+MySQL中你可以使用SELECT...INTO OUTFILE语句来简单的导出数据到文本文件上。
+使用 SELECT ... INTO OUTFILE 语句导出数据
+以下实例中我们将数据表 runoob_tbl 数据导出到 /tmp/tutorials.txt 文件中:
+mysql> SELECT * FROM runoob_tbl 
+    -> INTO OUTFILE '/tmp/tutorials.txt';
+你可以通过命令选项来设置数据输出的指定格式，以下实例为导出 CSV 格式：
+mysql> SELECT * FROM passwd INTO OUTFILE '/tmp/tutorials.txt'
+    -> FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+    -> LINES TERMINATED BY '\r\n';
+在下面的例子中，生成一个文件，各值用逗号隔开。这种格式可以被许多程序使用。
+SELECT a,b,a+b INTO OUTFILE '/tmp/result.text'
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+FROM test_table;
+SELECT ... INTO OUTFILE 语句有以下属性:
+LOAD DATA INFILE是SELECT ... INTO OUTFILE的逆操作，SELECT句法。为了将一个数据库的数据写入一个文件，使用SELECT ... INTO OUTFILE，为了将文件读回数据库，使用LOAD DATA INFILE。
+SELECT...INTO OUTFILE 'file_name'形式的SELECT可以把被选择的行写入一个文件中。该文件被创建到服务器主机上，因此您必须拥有FILE权限，才能使用此语法。
+输出不能是一个已存在的文件。防止文件数据被篡改。
+你需要有一个登陆服务器的账号来检索文件。否则 SELECT ... INTO OUTFILE 不会起任何作用。
+在UNIX中，该文件被创建后是可读的，权限由MySQL服务器所拥有。这意味着，虽然你就可以读取该文件，但可能无法将其删除。
+导出表作为原始数据
+mysqldump是mysql用于转存储数据库的实用程序。它主要产生一个SQL脚本，其中包含从头重新创建数据库所必需的命令CREATE TABLE INSERT等。
+使用mysqldump导出数据需要使用 --tab 选项来指定导出文件指定的目录，该目标必须是可写的。
+以下实例将数据表 runoob_tbl 导出到 /tmp 目录中：
+$ mysqldump -u root -p --no-create-info \
+            --tab=/tmp RUNOOB runoob_tbl
+password ******
+导出SQL格式的数据
+导出SQL格式的数据到指定文件，如下所示：
+$ mysqldump -u root -p RUNOOB runoob_tbl > dump.txt
+password ******
+以上命令创建的文件内容如下：
+-- MySQL dump 8.23
+--
+-- Host: localhost    Database: RUNOOB
+---------------------------------------------------------
+-- Server version       3.23.58
+
+--
+-- Table structure for table `runoob_tbl`
+--
+
+CREATE TABLE runoob_tbl (
+  runoob_id int(11) NOT NULL auto_increment,
+  runoob_title varchar(100) NOT NULL default '',
+  runoob_author varchar(40) NOT NULL default '',
+  submission_date date default NULL,
+  PRIMARY KEY  (runoob_id),
+  UNIQUE KEY AUTHOR_INDEX (runoob_author)
+) TYPE=MyISAM;
+
+--
+-- Dumping data for table `runoob_tbl`
+--
+
+INSERT INTO runoob_tbl 
+       VALUES (1,'Learn PHP','John Poul','2007-05-24');
+INSERT INTO runoob_tbl 
+       VALUES (2,'Learn MySQL','Abdul S','2007-05-24');
+INSERT INTO runoob_tbl 
+       VALUES (3,'JAVA Tutorial','Sanjay','2007-05-06');
+如果你需要导出整个数据库的数据，可以使用以下命令：
+$ mysqldump -u root -p RUNOOB > database_dump.txt
+password ******
+如果需要备份所有数据库，可以使用以下命令：
+$ mysqldump -u root -p --all-databases > database_dump.txt
+password ******
+--all-databases 选项在 MySQL 3.23.12 及以后版本加入。
+该方法可用于实现数据库的备份策略。
+将数据表及数据库拷贝至其他主机
+如果你需要将数据拷贝至其他的 MySQL 服务器上, 你可以在 mysqldump 命令中指定数据库名及数据表。
+在源主机上执行以下命令，将数据备份至 dump.txt 文件中:
+$ mysqldump -u root -p database_name table_name > dump.txt
+password *****
+如果完整备份数据库，则无需使用特定的表名称。
+如果你需要将备份的数据库导入到MySQL服务器中，可以使用以下命令，使用以下命令你需要确认数据库已经创建：
+$ mysql -u root -p database_name < dump.txt
+password *****
+你也可以使用以下命令将导出的数据直接导入到远程的服务器上，但请确保两台服务器是相通的，是可以相互访问的：</p>
+$ mysqldump -u root -p database_name \
+       | mysql -h other-host.com database_name
+
+
+
+       MySQL 导入数据
+       MySQL中可以使用两种简单的方式来导入MySQL导出的数据。
+       使用 LOAD DATA 导入数据
+       MySQL 中提供了LOAD DATA INFILE语句来插入数据。 以下实例中将从当前目录中读取文件 dump.txt ，将该文件中的数据插入到当前数据库的 mytbl 表中。
+       mysql> LOAD DATA LOCAL INFILE 'dump.txt' INTO TABLE mytbl;
+       　如果指定LOCAL关键词，则表明从客户主机上按路径读取文件。如果没有指定，则文件在服务器上按路径读取文件。
+       你能明确地在LOAD DATA语句中指出列值的分隔符和行尾标记，但是默认标记是定位符和换行符。
+       两个命令的 FIELDS 和 LINES 子句的语法是一样的。两个子句都是可选的，但是如果两个同时被指定，FIELDS 子句必须出现在 LINES 子句之前。
+       如果用户指定一个 FIELDS 子句，它的子句 （TERMINATED BY、[OPTIONALLY] ENCLOSED BY 和 ESCAPED BY) 也是可选的，不过，用户必须至少指定它们中的一个。
+       mysql> LOAD DATA LOCAL INFILE 'dump.txt' INTO TABLE mytbl
+         -> FIELDS TERMINATED BY ':'
+         -> LINES TERMINATED BY '\r\n';
+       LOAD DATA 默认情况下是按照数据文件中列的顺序插入数据的，如果数据文件中的列与插入表中的列不一致，则需要指定列的顺序。
+       如，在数据文件中的列顺序是 a,b,c，但在插入表的列顺序为b,c,a，则数据导入语法如下：
+       mysql> LOAD DATA LOCAL INFILE 'dump.txt' 
+           -> INTO TABLE mytbl (b, c, a);
+       使用 mysqlimport 导入数据
+       mysqlimport客户端提供了LOAD DATA INFILEQL语句的一个命令行接口。mysqlimport的大多数选项直接对应LOAD DATA INFILE子句。
+       从文件 dump.txt 中将数据导入到 mytbl 数据表中, 可以使用以下命令：
+       $ mysqlimport -u root -p --local database_name dump.txt
+       password *****
+       mysqlimport命令可以指定选项来设置指定格式,命令语句格式如下：
+       $ mysqlimport -u root -p --local --fields-terminated-by=":" \
+          --lines-terminated-by="\r\n"  database_name dump.txt
+       password *****
+       mysqlimport 语句中使用 --columns 选项来设置列的顺序：
+       $ mysqlimport -u root -p --local --columns=b,c,a \
+           database_name dump.txt
+       password *****
+       mysqlimport的常用选项介绍
+       选项	功能
+       -d or --delete	新数据导入数据表中之前删除数据数据表中的所有信息
+       -f or --force	不管是否遇到错误，mysqlimport将强制继续插入数据
+       -i or --ignore	mysqlimport跳过或者忽略那些有相同唯一 关键字的行， 导入文件中的数据将被忽略。
+       -l or -lock-tables	数据被插入之前锁住表，这样就防止了， 你在更新数据库时，用户的查询和更新受到影响。
+       -r or -replace	这个选项与－i选项的作用相反；此选项将替代 表中有相同唯一关键字的记录。
+       --fields-enclosed- by= char	指定文本文件中数据的记录时以什么括起的， 很多情况下 数据以双引号括起。 默认的情况下数据是没有被字符括起的。
+       --fields-terminated- by=char	指定各个数据的值之间的分隔符，在句号分隔的文件中， 分隔符是句号。您可以用此选项指定数据之间的分隔符。 默认的分隔符是跳格符（Tab）
+       --lines-terminated- by=str	此选项指定文本文件中行与行之间数据的分隔字符串 或者字符。 默认的情况下mysqlimport以newline为行分隔符。 您可以选择用一个字符串来替代一个单个的字符： 一个新行或者一个回车。
+       mysqlimport命令常用的选项还有-v 显示版本（version）， -p 提示输入密码（password）等。
