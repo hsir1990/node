13a645bce2f0dd29b367ea76af5226cdb2cb7f16
anke <245783065@qq.com>
2018-08-31 15:41:41 +0800

    提交

---
+++ weixing/httpServer.js
@@ -0,0 +1,20 @@
+//创建HTTP服务器
+
+//1. 加载http模块
+var http = require('http');
+
+//2. 创建http服务器
+// 参数: 请求的回调, 当有人访问服务器的时候,就会自动调用回调函数
+var server = http.createServer(function (request, response) {
+  console.log('有人访问了服务器')
+
+  //回调数据
+  response.write('Hello, My Love')
+  response.end()
+})
+
+//3. 绑定端口
+server.listen(9000)
+
+//4. 执行
+console.log('执行了3030')
